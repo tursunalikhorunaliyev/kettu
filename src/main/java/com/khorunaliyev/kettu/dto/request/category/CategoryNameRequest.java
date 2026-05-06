84a4abfee9fa12ffa03fd2f574945f4388fe14a4
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2026-05-06 17:30:23 +0500

    first_login and flyway migration has added

--- src/main/java/com/khorunaliyev/kettu/dto/request/category/CategoryNameRequest.java
+++ src/main/java/com/khorunaliyev/kettu/dto/request/category/CategoryNameRequest.java
@@ -11,4 +11,8 @@ public class CategoryNameRequest {
     @NotNull
     @Size(min = 1, max = 100)
     private String name;
+
+    @NotNull
+    @Size(min = 1, max = 100)
+    private String slug;
 }

002a2342ba1407af973201ef6592e0b6a4c4e043
khorunaliyev <khorunaliyev@gmail.com>
2026-05-31 19:43:58 +0500

    importing geo data logic has adopted new json files and i8l implementing local translation

--- src/main/java/com/khorunaliyev/kettu/services/geo/GeoService.java
+++ src/main/java/com/khorunaliyev/kettu/services/geo/GeoService.java
@@ -6,6 +6,8 @@ import com.khorunaliyev.kettu.dto.reponse.geo.GeoDataFromPoint;
 import com.khorunaliyev.kettu.repository.resource.DistrictRepository;
 import jakarta.persistence.Tuple;
 import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +16,18 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class GeoService {
     private final DistrictRepository districtRepository;
+    private final MessageSource messageSource;
 
     public GeoDataFromPoint geoData(double latitude, double longitude){
+        System.out.println(LocaleContextHolder.getLocale());
+        System.out.print("-----------------------------------");
         final Tuple data = districtRepository.findDistrictByGeoData(latitude, longitude);
         if(data == null){
             throw new ResourceNotFoundException("District not found");
         }
 
         Integer regionId = data.get("region_id", Integer.class);
-        String regionName = data.get("region_name", String.class);
+        String regionName = messageSource.getMessage(data.get("region_name", String.class), null, LocaleContextHolder.getLocale());
 
         Integer districtId = data.get("district_id", Integer.class);
         String districtName = data.get("district_name", String.class);
--- src/main/java/com/khorunaliyev/kettu/services/resource/RegionService.java
+++ src/main/java/com/khorunaliyev/kettu/services/resource/RegionService.java
@@ -12,6 +12,8 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -27,9 +29,10 @@ public class RegionService {
 
     private final RegionRepository regionRepository;
     private final CountryRepository countryRepository;
+    private final MessageSource messageSource;
 
     public ResponseEntity<Response> getByCountry(String country){
-        return ResponseEntity.ok(new Response("Success", regionRepository.findByCountryName(country).stream().map(regionInfo -> new IDNameItemCountDTO(regionInfo.getId(), regionInfo.getName(), regionInfo.getActiveItemCount()))));
+        return ResponseEntity.ok(new Response("Success", regionRepository.findByCountryName(country).stream().map(regionInfo -> new IDNameItemCountDTO(regionInfo.getId(), messageSource.getMessage(regionInfo.getName(), null, LocaleContextHolder.getLocale()), regionInfo.getActiveItemCount()))));
     }
 
     public ResponseEntity<Response> createRegion(Integer countryId, String name){
